a1aff920da1668bb008beee33d5bafa487302eba
programm-papa <kpavel_wrk@mail.ru>
2022-07-08 02:25:43 +0300

    create new page 404.php

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php $pageTitle = '404';
+
+require 'header.php' ?>
+
+<div class="error-page content-wrap">
+    <div class="error">
+        <div class="error-description">
+            <div class="title">Кажется, такой страницы больше нет</div>
+            <div class="description">Предлагаем перейти в наш каталог и попробовать наши наборы стейков из мраморной говядины</div>
+            <a href="" class="catalog-link">Выбирайте правильное мясо</a>
+        </div>
+    </div> 
+</div>
+<?php require 'footer.php' ?>
